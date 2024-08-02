4100be569aca753a67e70d6bed217425f9bfa7ee
luguoxiguazhuang <292632779@qq.com>
2024-08-02 17:01:42 +0800

    Completed lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,28 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    add t0, a0, x0
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    addi a1, a0, -1
+loop:
+    beq a1, x0, exit
+    jal ra, mult
+    addi a1, a1, -1
+    jal x0, loop
+exit:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jr ra
+
+mult:
+    add t1, a1, x0
+    add t2, a0, x0
+    addi t3, x0, 1
+newloop:
+    beq t1, t3, newexit
+    add a0, a0, t2
+    addi t1, t1, -1
+    jal x0, newloop
+newexit:
+    jr ra
